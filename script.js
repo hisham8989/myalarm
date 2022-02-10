0a111dcc4fcf0b4c77c3e34ecd859568d81e0fe2
hisham8989 <khanhisham25@gmail.com>
2022-02-10 12:54:50 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-let currentTime = document.getElementById("current-time");
-let currentTimeMeridian = document.querySelector("#time-meridian");
-let timeSpan = document.querySelector("#time-span");
-let setHour = document.querySelector("#set-hour");
+let currentTime = document.getElementById('current-time')
+let currentTimeMeridian = document.querySelector('#time-meridian')
+let timeSpan = document.querySelector('#time-span')
+let setHour = document.querySelector('#set-hour')
 
 function getCurrentTime() {
-    let today = new Date();
-    let hour = today.getHours() < 10 ? "0" + today.getHours() : today.getHours();
-    let minute =
-      today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
-    let second =
-      today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
-  
-    if (hour > 12) {
-      hour = hour - 12;
-    }
-    return hour + ":" + minute + ":" + second;
+  let today = new Date()
+  let hour = today.getHours() < 10 ? '0' + today.getHours() : today.getHours()
+  let minute =
+    today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes()
+  let second =
+    today.getSeconds() < 10 ? '0' + today.getSeconds() : today.getSeconds()
+
+  if (hour > 12) {
+    hour = hour - 12
   }
+  return hour + ':' + minute + ':' + second
+}
 
 function setCurrentTime() {
-    let currentTimeString = getCurrentTime();
-    if (currentTimeString.split(":")[0] < 12) {
-      currentTimeMeridian.innerText = "AM";
-    }
-    timeSpan.innerText = currentTimeString;
+  let currentTimeString = getCurrentTime()
+  if (currentTimeString.split(':')[0] < 12) {
+    currentTimeMeridian.innerText = 'AM'
   }
-
-function hourList(){
-  let time = getCurrentTime();
-  for(var i = 0;i<time.split(":")[0];i++){
-    
-  }
+  timeSpan.innerText = currentTimeString
 }
-let interval = setInterval(setCurrentTime, 1000);
+
+function hourList() {
+  // let initial_hour_list = getCurrentTime()
+  // for (var i = initial_hour_list.split(':')[0]; i <=12 ; i++) {
+  //     console.log(i);
+  // }
+  
+}
+let interval = setInterval(setCurrentTime, 1000)
+
+hourList()
